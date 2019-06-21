51f5c351e37f0aa524d63279fd1b7d08cb421f0b
G.K Raja <gkrajareddy@gmail.com>
2019-06-21 19:20:06 +0530

    basic app

---
+++ js/javaScript.js
@@ -0,0 +1,11 @@
+
+function calcNumbers(result){
+  if (true) {
+    form.displayResult.value=form.displayResult.value+result;
+
+  }else {
+    form.displayResult.value=form.displayResult.value+result;
+
+  }
+
+}
